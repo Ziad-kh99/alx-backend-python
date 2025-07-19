2937b7656397a02589bf038248c633a1fd251475
Ziad Khaled <ziad.dev99@gmail.com>
2025-07-20 02:19:30 +0300

    Write unit test to access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,13 +5,20 @@ from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
         ({}, ("a",), "a"),
         ({"a": 1}, ("a", "b"), "b"),
     ])
-
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
     def test_access_nested_map_exception(self, nested_map, path, expected_key):
         with self.assertRaises(KeyError) as cm:
             utils.access_nested_map(nested_map, path)
