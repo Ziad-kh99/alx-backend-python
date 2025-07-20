b1adb89d97f01b03ebf12def8e89a78ead24eba1
Ziad Khaled <ziad.dev99@gmail.com>
2025-07-20 08:25:24 +0300

    Wirte unit test for get_json method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 import unittest
 import utils
 from parameterized import parameterized
-
+from unittest.mock import patch, Mock
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -24,3 +24,26 @@ class TestAccessNestedMap(unittest.TestCase):
             utils.access_nested_map(nested_map, path)
 
         self.assertEqual(str(cm.exception), f"'{expected_key}'")
+
+
+class TestGetJson(unittest.TestCase):
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+
+        for test_url, test_payload in test_cases:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = utils.get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
+            # Reset mock between iterations
+            mock_get.reset_mock()
+
